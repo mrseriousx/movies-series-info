79938aeb3f57481a7f98d019053105489e00cf58
Murphy <haihuynhng@gmail.com>
2020-03-07 19:52:21 +0700

    fix  for deploy v1.6

---
+++ build/precache-manifest.e0f9538b3a8c6931fa63ca2861c9051d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525009fef6cea2580d4483c3f6ba93f5",
+    "url": "/movies-series-info/index.html"
+  },
+  {
+    "revision": "ff4d2ad5033dad16b7f4",
+    "url": "/movies-series-info/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "c919b0de6a3815d9d88a",
+    "url": "/movies-series-info/static/js/2.0921ca65.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/movies-series-info/static/js/2.0921ca65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4d2ad5033dad16b7f4",
+    "url": "/movies-series-info/static/js/main.0ed4bf7f.chunk.js"
+  },
+  {
+    "revision": "b37f11f258d327f66dd5",
+    "url": "/movies-series-info/static/js/runtime-main.c48687d0.js"
+  }
+]);
