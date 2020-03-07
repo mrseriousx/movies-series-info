8c63fe356dec2339c78e676218d13f9dfad69617
Murphy <haihuynhng@gmail.com>
2020-03-07 19:30:47 +0700

    Updates

--- precache-manifest.480af9b511832f5521dbcd7bd31253d1.js
+++ precache-manifest.480af9b511832f5521dbcd7bd31253d1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17b2ca81b24451b0028b07394cd66510",
+    "revision": "bcd620405a58253b82cf1ae24693701e",
     "url": "/index.html"
   },
   {
-    "revision": "39c96dd565bad0cb5ec3",
+    "revision": "76adbf783cd0d114328a",
     "url": "/static/css/main.d20d7e65.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0921ca65.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "39c96dd565bad0cb5ec3",
-    "url": "/static/js/main.9a842847.chunk.js"
+    "revision": "76adbf783cd0d114328a",
+    "url": "/static/js/main.a60a1ee2.chunk.js"
   },
   {
     "revision": "3e297d12526c7dcdd4f5",
